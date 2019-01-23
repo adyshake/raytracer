48c1914d8ac3d5089cb8b45713c09630aa16ecfa
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2019-01-23 12:35:49 +0530

    Added initial ray bounce

--- main.cpp
+++ main.cpp
@@ -1,36 +1,65 @@
 #include <iostream>
+#include <time.h>
+
 #include "vec3.h"
 #include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "camera.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-
+float get_rand() {
+    return (float)rand() / (RAND_MAX + 1);
 }
 
-vec3 color(const ray &r) {
-    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-    if (t > 0.0f) {
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3(get_rand(), get_rand(), get_rand()) - vec3(1.0, 1.0, 1.0);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+vec3 color(const ray &r, hitable *world) {
+    hit_record rec;
+    if (world->hit(r, 0.001f, FLT_MAX, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target-rec.p), world);
     }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5f *(unit_direction.y() + 1.0f);
-    return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f *(unit_direction.y() + 1.0f);
+        return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
+    }
 }
 
 int main() {
+    srand((unsigned)time(NULL));
+
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
+    hitable *list[2];
+    list[0] = new sphere(vec3(0,0,-1), 0.5);
+    list[1] = new sphere(vec3(0,-100.5,-1), 100);
+    hitable *world = new hitable_list(list, 2);
+    camera cam;
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i)/float(nx);
-            float v = float(j)/float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + get_rand()) / float(nx);
+                float v = float(j + get_rand()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2);
+                col += color(r, world);
+            }
+            col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); //Applying Gamma 2
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
